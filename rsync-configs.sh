97c6457a0c0272ffd75db3859ae593f6dcb5dbf9
Leon Matthews <leon@digitaladvisor.nz>
2024-04-17 10:49:31 +1200

    Update config for Rook 2020 MK1

--- rsync-configs.sh
+++ rsync-configs.sh
@@ -6,10 +6,11 @@
 
 set -o errexit
 set -o nounset
+set -o xtrace
 
 
-REMOTE_FOLDER='~/printer_data/config/'
-LOCAL_FOLDER='~/Projects/klipper-config/'
+REMOTE_FOLDER="/home/leon/printer_data/config/"
+LOCAL_FOLDER="/home/leon/Projects/klipper-config/"
 
 rsync rook.local:"$REMOTE_FOLDER" "${LOCAL_FOLDER}rook-2020-mk1/" \
-    -rtPWh --copy-links --stats --delete-delay -n
+    -rtPWh --copy-links --stats --delete-delay
